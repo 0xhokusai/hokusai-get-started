509d7a8d875d8965370597465004a2ce037754a0
wan-nyan-wan <distributed.system.love@gmail.com>
2021-08-24 17:21:55 +0900

    init

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import fs from 'fs'
+import { NFTStorage, File, toGatewayURL } from 'nft.storage'
+require('dotenv').config()
+
+const endpoint = toGatewayURL('https://api.nft.storage')
+const token = process.env.API_KEY || ''
+
+async function main() {
+  const storage = new NFTStorage({ endpoint, token })
+  const metadata = await storage.store({
+    name: 'nft.storage store test',
+    description:
+      'Using the nft.storage metadata API to create ERC-1155 compatible metadata.',
+    image: new File([await fs.promises.readFile('hokusai.png')], 'hokusai.png', {
+      type: 'image/png',
+    })
+  })
+
+  console.log('IPFS URL for the metadata:', metadata.url)
+}
+
+main()
