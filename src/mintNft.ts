b63dd3aacf107c764c30521e1846a314997f1705
wan-nyan-wan <distributed.system.love@gmail.com>
2021-09-10 14:17:10 +0900

    fix code

--- src/mintNft.ts
+++ src/mintNft.ts
@@ -1,31 +1,50 @@
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 
-require('dotenv').config()
+require("dotenv").config();
 
-const baseUrl = "https://hokusai-api-gateway-1mtclftl.an.gateway.dev"
+const baseUrl = "https://polygon.hokusai.app";
 
-const mintNft = async (baseUrl: string, apiKey: string, contractId: string, to: string, tokenUri: string) => {
-    const path = `/v1/nfts/${contractId}/mint`
-    const url = new URL(baseUrl + path)
-    const params = { key: apiKey }
-    const requestBody = { to, tokenUri, }
-    url.search = new URLSearchParams(params).toString()
-    const res = await fetch(
-        url.toString(),
-        {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(requestBody)
-        })
-    return res.json()
-}
+const mintNft = async (
+  baseUrl: string,
+  apiKey: string,
+  contractId: string,
+  to: string,
+  tokenUri: string
+) => {
+  const path = `/v1/nfts/${contractId}/mint`;
+  const url = new URL(baseUrl + path);
+  const params = { key: apiKey };
+  const requestBody = { to, tokenUri };
+  url.search = new URLSearchParams(params).toString();
+  const res = await fetch(url.toString(), {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(requestBody),
+  });
+  if (res.status != 200) {
+    throw new Error(await res.text());
+  }
+  return res.json();
+};
 
-const argv = process.argv.slice(2)
+const argv = process.argv.slice(2);
 
 if (argv.length !== 2) {
-    console.log("Usage: node mintNft.ts <to> <tokenUri>")
-    process.exit(1)
+  console.log("Usage: node mintNft.ts <to> <tokenUri>");
+  process.exit(1);
 }
 
-mintNft(baseUrl, process.env.HOKUSAI_API_KEY || '', process.env.HOKUSAI_CONTRACT_ID || '', argv[0], argv[1])
-    .then((res) => { console.log(res) })
+mintNft(
+  baseUrl,
+  process.env.HOKUSAI_API_KEY || "",
+  process.env.HOKUSAI_CONTRACT_ID || "",
+  argv[0],
+  argv[1]
+)
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
--- src/storeMetadata.ts
+++ src/storeMetadata.ts
@@ -1,24 +1,30 @@
-import fs from 'fs'
-import { NFTStorage, File, toGatewayURL } from 'nft.storage'
+import fs from "fs";
+import { NFTStorage, File, toGatewayURL } from "nft.storage";
 
-require('dotenv').config()
+require("dotenv").config();
 
-const endpoint = toGatewayURL('https://api.nft.storage')
-const token = process.env.NFT_STORAGE_API_KEY || ''
+const endpoint = toGatewayURL("https://api.nft.storage");
+const token = process.env.NFT_STORAGE_API_KEY || "";
 
 async function main() {
-  const storage = new NFTStorage({ endpoint, token })
+  const storage = new NFTStorage({ endpoint, token });
   const metadata = await storage.store({
-    name: 'nft.storage store test',
+    name: "nft.storage store test",
     description:
-      'Using the nft.storage metadata API to create ERC-1155 compatible metadata.',
-    image: new File([await fs.promises.readFile('hokusai.png')], 'hokusai.png', {
-      type: 'image/png',
-    })
-  })
+      "Using the nft.storage metadata API to create ERC-1155 compatible metadata.",
+    image: new File(
+      [await fs.promises.readFile("hokusai.png")],
+      "hokusai.png",
+      {
+        type: "image/png",
+      }
+    ),
+  });
 
-  const url = new URL(metadata.url)
-  console.log(`HTTPS URL for the metadata: https://dweb.link/ipfs/${url.hostname}${url.pathname}`)
+  const url = new URL(metadata.url);
+  console.log(
+    `HTTPS URL for the metadata: https://dweb.link/ipfs/${url.hostname}${url.pathname}`
+  );
 }
 
-main()
+main();
