3401dc7c9717ffaa6c5cc4d14e13a138d71b0424
tocoteron <tocoteron@gmail.com>
2021-09-14 12:34:26 +0900

    Create a sample program to use meta-tx

---
+++ src/lib/MetaTx.ts
@@ -0,0 +1,66 @@
+import { ethers } from 'ethers';
+import {
+	Message,
+  MessageWithSignature,
+  createTypedDataV4
+} from './TypedData';
+import ForwarderAbi from './abis/MinimalForwarder.json';
+import HokusaiAbi from './abis/ERC721WithRoyaltyMetaTx.json';
+
+export async function getMetaTxMessageWithSignature(
+  walletPrivateKey: string,
+  contractAddress: string,
+  forwarderAddress: string,
+  toAddress: string,
+  tokenId: number,
+) {
+  const RPC = "https://rpc-mumbai.matic.today"
+  const provider = new ethers.providers.JsonRpcProvider(RPC)
+  const signer = new ethers.Wallet(walletPrivateKey)
+
+  const { chainId } = await provider.getNetwork()
+  const address = await signer.getAddress()
+
+  // Setup contracts
+  const forwarder = new ethers.Contract(
+    forwarderAddress,
+    ForwarderAbi.abi,
+    provider
+  );
+  const hokusaiInterface = new ethers.utils.Interface(HokusaiAbi.abi);
+
+  // Create tranferFrom data
+  const data = hokusaiInterface.encodeFunctionData('transferFrom', [
+    address,
+    toAddress,
+    tokenId,
+  ]);
+
+  // Create meta transaction message
+  const message: Message = {
+    from: address,
+    to: contractAddress,
+    value: 0,
+    gas: 1e6,
+    nonce: (await forwarder.getNonce(address)).toNumber(),
+    data,
+  }
+
+  // Create typedDataV4
+  const typedData = createTypedDataV4(
+    chainId,
+    forwarderAddress,
+    message
+  );
+
+  // Sign message
+  const signature =  await signer._signTypedData(
+    typedData.domain,
+    typedData.types,
+    typedData.message
+  )
+  const messageWithSignature: MessageWithSignature = {...message, signature}
+
+  return messageWithSignature
+}
+
---
+++ src/transferNft.ts
@@ -0,0 +1,75 @@
+import fetch from "node-fetch";
+import { MessageWithSignature } from './lib/TypedData';
+import { getMetaTxMessageWithSignature } from './lib/MetaTx';
+
+require("dotenv").config();
+
+const baseUrl = "https://mumbai.hokusai.app";
+
+type TransferRequest = {
+  request: MessageWithSignature
+}
+
+async function transferNft(
+  baseUrl: string,
+  apiKey: string,
+  contractId: string,
+  messageWithSignature: MessageWithSignature
+) {
+  const path = `/v1/nfts/${contractId}/transfer`;
+  const url = new URL(`${baseUrl}${path}`);
+  const params = { key: apiKey };
+  const requestBody: TransferRequest = { request: messageWithSignature }
+
+  url.search = new URLSearchParams(params).toString();
+
+  const res = await fetch(url.toString(), {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(requestBody),
+  });
+
+  if (res.status != 200) {
+    console.log(res)
+    throw new Error(await res.text());
+  }
+
+  return res.json();
+}
+
+async function main() {
+  const argv = process.argv.slice(2);
+
+  if (argv.length !== 2) {
+    console.log("Usage: node mintNft.ts <to> <tokenId>");
+    process.exit(1);
+  }
+
+  const walletPrivateKey = process.env.WALLET_PRIVATE_KEY || ""
+  const apiKey = process.env.HOKUSAI_API_KEY || ""
+  const contractId = process.env.HOKUSAI_CONTRACT_ID || ""
+  const contractAddress = process.env.HOKUSAI_CONTRACT_ADDRESS || ""
+  const forwarderAddress = "0x04D2fA199bd15a6D118B5481dc646cbfCBED425D" // Mumbai(testnet) forwarder address
+  const toAddress = argv[0] || ""
+  const tokenId = Number(argv[1]) || 0
+
+  try {
+    const messageWithSignature = await getMetaTxMessageWithSignature(
+      walletPrivateKey,
+      contractAddress,
+      forwarderAddress,
+      toAddress,
+      tokenId
+    )
+    console.log("Message with signature:")
+    console.log(messageWithSignature)
+
+    const res = await transferNft(baseUrl, apiKey, contractId, messageWithSignature)
+    console.log("Transfer result:")
+    console.log(res)
+  } catch(error) {
+    console.log(error)
+  }
+}
+
+main()
