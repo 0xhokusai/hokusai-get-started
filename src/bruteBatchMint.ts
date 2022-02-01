267a757f35fb81a3af87e7c256d01ce39b01cd52
jaguarmouse <kazune.haraguchi.aboon@gmail.com>
2022-02-01 14:02:32 +0900

    fixed main for ARMC use

--- src/bruteBatchMint.ts
+++ src/bruteBatchMint.ts
@@ -4,10 +4,6 @@ import { ethers } from "ethers"
 
 require("dotenv").config();
 
-const baseUrl = "https://polygon.hokusai.app";
-const adminAddress = '0x48CbAAAb40C6f43662898A782Fb5Ff92b04DA6C1'
-const RPC = 'https://polygon-mainnet.infura.io/v3/07a9762f17d34a56b8dd440e283126b8'
-
 const mintNft = async (
   baseUrl: string,
   apiKey: string,
@@ -33,6 +29,14 @@ const mintNft = async (
   return res.json();
 };
 
+const baseUrl = "https://polygon.hokusai.app";
+const RPC = 'https://polygon-mainnet.infura.io/v3/07a9762f17d34a56b8dd440e283126b8'
+const API_KEY = process.env.HOKUSAI_API_KEY
+const CONTRACT_ID = process.env.HOKUSAI_CONTRACT_ID
+
+if (!(API_KEY && CONTRACT_ID))
+  process.exit(1)
+
 const waitForTx = async (txHash: string) => {
   const provider = new ethers.providers.JsonRpcProvider(RPC);
   return await provider.waitForTransaction(txHash)
@@ -40,29 +44,40 @@ const waitForTx = async (txHash: string) => {
 
 console.log(`baseUrl: ${baseUrl}`);
 
-const list = JSON.parse(fs.readFileSync('metadata.json', 'utf8'))
+// const list = JSON.parse(fs.readFileSync('metadata.json', 'utf8'))
+// const keys = JSON.parse(fs.readFileSync('keys.json', 'utf8')).mainnet as { key: string, id: string, author: string }[]
 
-const keys = JSON.parse(fs.readFileSync('keys.json', 'utf8')).mainnet as { key: string, id: string, author: string }[]
+const data = JSON.parse(fs.readFileSync('data.json', 'utf8')) as { key: number, hash: string, address: string }[]
 
 const main = async () => {
   try {
-    for (const elem of keys) {
-      const hashList = list[elem.author] as { key: number, hash: string }[]
-      const hashes = hashList.sort((f, s) => f.key - s.key)
-      console.log(elem.author)
-      for (const hash of hashes) {
-        const url = "ipfs://" + hash.hash
-        console.log(`     ${hash.key}: ` + url)
-        const res = await mintNft(baseUrl, elem.key, elem.id, adminAddress, url)
-        if (res.txHash) {
-          console.log(`Transaction sent: ${res.txHash}`)
-          await waitForTx(res.txHash)
-          console.log(`Transaction mined: ${res.txHash}`)
-        } else {
-          throw new Error('unknown error')
-        }
+    for (const item of data) {
+      const url = "ipfs://" + item.hash
+      console.log(`     ${item.key}: ` + url)
+      const res = await mintNft(baseUrl, API_KEY, CONTRACT_ID, item.address, url)
+      if (res.txHash) {
+        console.log(`Transaction sent: ${res.txHash}`)
+        await waitForTx(res.txHash)
+        console.log(`Transaction mined: ${res.txHash}`)
+      } else {
+        throw new Error('unknown error')
       }
-    }
+    // for (const elem of keys) {
+    //   const hashList = list[elem.author] as { key: number, hash: string }[]
+    //   const hashes = hashList.sort((f, s) => f.key - s.key)
+    //   console.log(elem.author)
+    //   for (const hash of hashes) {
+    //     const url = "ipfs://" + hash.hash
+    //     console.log(`     ${hash.key}: ` + url)
+    //     const res = await mintNft(baseUrl, elem.key, elem.id, adminAddress, url)
+    //     if (res.txHash) {
+    //       console.log(`Transaction sent: ${res.txHash}`)
+    //       await waitForTx(res.txHash)
+    //       console.log(`Transaction mined: ${res.txHash}`)
+    //     } else {
+    //       throw new Error('unknown error')
+    //     }
+    //   }
     // const txHash = '0x82284b02c7e5aa70bb259caddb47ac589f06d23810f7a663ee3c494c2adc359a'
     // const provider = new ethers.providers.JsonRpcProvider(RPC)
     // const transaction = await provider.getTransaction(txHash)
@@ -72,6 +87,7 @@ const main = async () => {
     // console.log(`confirmations: ${transaction.confirmations}`)
     // const reciept = await waitForTx(txHash)
     // console.log(reciept)
+    }
   } catch(err) {
     console.error(err)
   }
